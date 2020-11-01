f61aa59bcd2a4d5249409bcd241363a60bea9e4f
seyikayode <oluwaseyikayode25@gmail.com>
2020-11-01 01:40:12 +0100

    commit

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "https://djangoreact-api.herokuapp.com/api",
     timeout: 5000,
     headers: {
         "Authorization": "JWT " + localStorage.getItem("access_token"),
